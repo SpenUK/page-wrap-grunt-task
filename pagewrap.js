2f5820d697df45c72662992f98dec7ffe2939c23
Spen Taylor <spen_@hotmail.co.uk>
2015-02-23 12:37:51 +0000

    Initial commit, working nicely but not bullet proof

---
+++ pagewrap.js
@@ -0,0 +1,54 @@
+/*
+ * html-page-wrap
+ *
+ * 2015 Spen Taylor
+ *
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  var path = require('path');
+  var fs = require('fs');
+  var chalk = require('chalk');
+
+  grunt.registerMultiTask('pagewrap', '-', function() {
+
+    var header = (this.data.wrapper && this.data.wrapper.header) ? this.data.wrapper.header : false;
+    var footer = (this.data.wrapper && this.data.wrapper.footer) ? this.data.wrapper.footer : false;
+
+    var headerContents = header ? getContents(header) : '';
+    var footerContents = footer ? getContents(footer) : '';
+
+      this.files.filter(function(filePair){
+        return (filePair.src != header && filePair.src != footer);
+      }).forEach(function(filePair){
+
+        filePair.src.forEach(function(file){
+          var fileContents = getContents(file);
+          
+          if (fileContents) {
+            var builtContents = [headerContents,fileContents,footerContents].join('\n');
+            grunt.file.write(filePair.dest, builtContents);
+
+            grunt.log.writeln('Produced: "'+ filePair.dest + '".');
+          } else {
+            grunt.log.warn('Source file "' + filepath + '" not found.');
+          };
+
+        });
+      });
+
+  });
+
+  function getContents(file) {
+    grunt.file.exists(file);
+    if (!grunt.file.exists(file)) {
+      return false;
+    } else {
+      return grunt.file.read(file);
+    };
+  }
+    
+};
---
+++ readme.md
@@ -0,0 +1,28 @@
+PageWrap Grunt Task
+===============
+
+Takes a collection of content pages and wraps them with content from a header and footer file.
+Quite useless in most cases, since you would normally use partials!
+
+## How to use
+
+Using this task is quite straight forward, set a cwd, src files, destination and a wrappers object that includes both a header and footer file.
+
+Either the header or footer files can be omitted if they are not neededå
+
+Example config:
+
+```
+pagewrap: {
+  example: {
+    expand: true,
+    cwd: 'app-directory/html/',
+    src: ['**/*.html' ], // processes all .html files within the 'cwd' directory
+    dest: 'destination-directory/', // creates new files in the destination directory, with the same structure as in the 'cwd' directory
+  
+    wrapper: {
+      header: 'app-directory/html/wrapper/header.html',
+      footer: 'app-directory/html/wrapper/footer.html'
+    }
+  }
+}
